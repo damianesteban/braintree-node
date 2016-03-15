3535e93b62916e61ff3647f7e395d59af31931ab
varunjayaraman <varun.jayaraman@gmail.com>
2016-03-15 00:53:01 -0400

    tests

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ module.exports = function(config) {
   config.environment = braintree.Environment[handleEnv(config.environment)];
   // instantiate gateway object with configuration
   const gateway = braintree.connect(config);
+
+  /**
+   * Generate a client token to be sent to client
+   * @return {Promise} with result of `gateway.clientToken.generate`
+   */
   gateway.generateClientToken = function() {
     return new Promise((resolve, reject) => {
       this.clientToken.generate({}, function(error, result) {
@@ -39,6 +44,187 @@ module.exports = function(config) {
         }
         return resolve(result);
       });
+    });
+  };
+
+  /**
+   *  `gateway.transaction.sale`
+   * @param {Number|String} amount, required
+   * @param {String} nonce, required
+   * @param {options} options, optional
+   */
+
+  gateway.createTransaction = function(amount, nonce, options) {
+    return new Promise((resolve, reject) => {
+      if (!amount) {
+        return reject(new Error('Amount required to create transaction'));
+      }
+      if (!nonce) {
+        return reject(new Error('Nonce required to create transaction'));
+      }
+
+      const params = {
+        amount: amount,
+        paymentMethodNonce: nonce,
+        options: options
+      };
+
+      this.transaction.sale(params, function(error, result) {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+  };
+
+  /**
+   * `gateway.customer.find`
+   * @param {String} id, required
+   * @return {Promise}
+   */
+
+   gateway.findCustomer = function(id) {
+    return new Promise((resolve, reject) => {
+      if (!id) {
+        return reject(new Error('id required to find customer'));
+      }
+      this.customer.find(id, function(error, result) {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+   };
+
+  /**
+   * `gateway.customer.create`
+    * @param {user} user object with various paramaters
+    *   if no `id` property exists on user, default to braintree's
+    *   randomly generated id
+    *   to store a payment method along with the customer, be sure to include
+    *   a `paymentMethodNonce` property on the user object with the client nonce
+    * @return {Promise}
+   */
+  gateway.createCustomer = function(user) {
+    return new Promise((resolve, reject) => {
+      this.customer.create(user, function(error, result) {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+  };
+
+  /**
+   * `gateway.customer.update`
+   * @param {String} id, required, braintree id of user
+   * @param {update} update object
+   * @return {Promise}
+   */
+
+  gateway.updateCustomer = function(id, update) {
+    return new Promise((resolve, reject) => {
+      if (!id) {
+        return reject(new Error('id required to update customer'));
+      }
+      this.customer.update(id, update, function(error, result) {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+  };
+
+  /**
+   * `gateway.customer.delete`
+   * @param {String} id, braintree id of user to delete
+   * @return {Promise}
+   */
+
+  gateway.deleteCustomer = function(id) {
+    return new Promise((resolve, reject) => {
+      if (!id) {
+        return reject(new Error('id required to delete customer'));
+      }
+      this.customer.delete(id, function(error, result) {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+
+  /**
+   * @param {User} user object to update|create
+   * @param {Boolean} upsert, create new document if not found
+   * @return {Promise}
+   */
+  gateway.findOneAndUpdate = function(user, upsert) {
+    return new Promise((resolve, reject) => {
+      this.customer.update(user.id, user, (error, result) => {
+        if (error.type === 'notFoundError' && upsert) {
+          this.customer.create(user, (error, result) => {
+            if (error) {
+              return reject(error);
+            }
+            return resolve(result);
+          });
+        } else if (error) {
+          return reject(error);
+        } else {
+          return resolve(result);
+        }
+      });
+    });
+  };
+
+  /**
+   * @param {Array} user objects to create, required
+   * @return {Promise}
+   */
+
+  gateway.createMultipleCustomers = function(users) {
+    return new Promise((resolve, reject) => {
+      if (!users) {
+        return reject(new Error('users required'));
+      }
+      if (!Array.isArray(users)) {
+        users = [users];
+      }
+
+      const promises = [];
+      for (var i = 0; i < users.length; i++) {
+        promises.push(this.createCustomer(users[i]));
+      };
+
+      return resolve(Promise.all(promises));
+    });
+  };
+
+  /**
+   * @param {Array} array of user ids, required
+   * @return {Promise}
+   */
+  gateway.deleteMultipleCustomers = function(users) {
+    return new Promise((resolve, reject) => {
+      if (!users) {
+        return reject(new Error('users required'));
+      }
+      if (!Array.isArray(users)) {
+        users = [users];
+      }
+
+      const promises = [];
+      for (var i = 0; i < users.length; i++) {
+        promises.push(this.deleteCustomer(users[i].id));
+      }
+      return resolve(Promise.all(promises));
     });
   };
 
--- test.js
+++ test.js
@@ -1,19 +1,90 @@
 'use strict';
 const co = require('co');
 const _ = require('lodash');
+const fs = require('fs');
+const yaml = require('js-yaml');
 const assert = require('assert');
 const config = require('./config');
 const gateway = require('./')(config);
 
+const FAKE_DATA_PATH = 'fake.yml';
 describe('braintree wrapper', function() {
+  const fakeData = yaml.safeLoad(fs.readFileSync(FAKE_DATA_PATH));
+  const user = {
+    id: 'unique123'
+  };
+
+  beforeEach(function(done) {
+    co(function*() {
+      try { yield gateway.deleteCustomer(user.id); } catch(error) {}
+      done();
+    });
+  });
 
   it('generates a clientToken', done => co(function*() {
     const response = yield gateway.generateClientToken();
-    console.log(clientTokenResponse);
     assert.ok(response.success);
     assert.ok(_.isString(response.clientToken));
     done();
   }).catch(done));
 
+  it('creates a sale', done => co(function*() {
+    const response = yield gateway.createTransaction(15, fakeData.nonces.valid.nonce);
+    assert.ok(response.success);
+    assert.equal(response.transaction.amount, '15.00');
+    done();
+  }).catch(done));
+
+  it('creates a customer', done => co(function*() {
+    const response = yield gateway.createCustomer(user);
+    assert.ok(response.success);
+    assert.equal(response.customer.id, 'unique123');
+    done();
+  }).catch(done));
+
+  it('can find a customer', done => co(function*() {
+    try {
+      yield gateway.findCustomer(user.id);
+    } catch (error) {
+      assert.equal(error.type, 'notFoundError');
+    }
+    yield gateway.createCustomer(user);
+    const response = yield gateway.findCustomer(user.id);
+    assert.equal(response.id, 'unique123');
+    done();
+  }).catch(done));
+
+  it('can update a customer', done => co(function*() {
+    yield gateway.createCustomer(user);
+    const response = yield gateway.updateCustomer(user.id, {firstName: 'chicken'});
+    assert.ok(response.success);
+    assert.equal(response.customer.firstName, 'chicken');
+    done();
+  }).catch(done));
+
+  it('can upsert a customer', function(done) {
+    this.timeout(5000);
+    co(function*() {
+      user.lastName = 'bob';
+      const response = yield gateway.findOneAndUpdate(user, true);
+      assert.ok(response.success);
+      assert.equal(response.customer.lastName, 'bob');
+      done();
+    }).catch(done);
+  });
+
+  it('can create and delete multiple customers', function(done) {
+    this.timeout(5000);
+    co(function*() {
+      var users = [{id: 'boogly1'}, {id: 'boogly2'}, {id: 'boogly3'}];
+
+      const newUsers = yield gateway.createMultipleCustomers(users);
+      console.log(newUsers);
+
+      yield gateway.deleteMultipleCustomers(users);
+      done();
+
+    }).catch(done);
+  });
 
 });
